9e3e09ceb0c78138530f62c4ad9fbaf3ee5960ea
Cowwee90 <101339470+Cowwee90@users.noreply.github.com>
2023-07-19 18:29:59 +0800

    can fetch json from api and parse into table

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+//import React, { useEffect, useState } from "react";
+import { StudentTable } from "./student-table.jsx";
+
+const App = () => {
+  return StudentTable();
+};
+
+export default App;
---
+++ src/student-table.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+
+export function StudentTable() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const headers = {
+          "User-Agent": "testingAgent",
+        };
+        const response = await fetch(
+          "https://certification-api.glitch.me/students",
+          {
+            headers,
+          }
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fetching JSON with React</h1>
+      {data ? (
+        //<pre>{JSON.stringify(data, null, 2)}</pre>
+        showJsonInTable(data)
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+}
+
+function showJsonInTable(JsonData) {
+  const DisplayData = JsonData.map((info) => {
+    return (
+      <tr>
+        <td>{info.id}</td>
+        <td>{info.sname}</td>
+        <td>{info.birthday}</td>
+        <td>{info.highest_level}</td>
+        <td>{info.best_grade}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>sname</th>
+            <th>birthday</th>
+            <th>highest_level</th>
+            <th>best_grade</th>
+          </tr>
+        </thead>
+        <tbody>{DisplayData}</tbody>
+      </table>
+    </div>
+  );
+}
