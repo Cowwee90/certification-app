5e58be367f50748912515802da6516c1e362d460
Algebras <65031038+Algebras@users.noreply.github.com>
2023-07-19 21:02:18 -0600

    test form half done

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,15 @@
 //import React, { useEffect, useState } from "react";
 import { StudentTable } from "./student-table.jsx";
+import { MyForm } from "./test-form.jsx";
+
 
 const App = () => {
-  return StudentTable();
+  return (
+    <div>
+    <MyForm />
+    <StudentTable />
+  </div>);
 };
+
 
 export default App;
