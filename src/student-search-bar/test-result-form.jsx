c84d3afc41aa066108f9c04466e2accf4384b0ca
Cowwee90 <101339470+Cowwee90@users.noreply.github.com>
2023-07-25 13:12:34 +0800

    click name will appear

--- src/student-search-bar/test-result-form.jsx
+++ src/student-search-bar/test-result-form.jsx
@@ -1,39 +1,66 @@
 import { useState } from "react";
-import { SearchBar } from "./searchbar.jsx";
-import { SearchResultsList } from "./search-name-dropdown.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export function TestResultForm() {
-  const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-  const allResults = async () => {
-    try {
-      const response = await fetch(
-        `https://certification-api.glitch.me/students`
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+  const onSearch = (searchTerm) => {
+    console.log("search", searchTerm);
   };
 
-  const handleSearch = async () => {
-    try {
-      const response = await fetch(
-        `https://certification-api.glitch.me/students?search_query=${query}`
-      );
-      const data = await response.json();
-      setResults(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+  const fetchData = (value) => {
+    fetch("http://certification-api.glitch.me/students")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.birthday &&
+            user.sname &&
+            user.sname.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
   };
 
+  const handleChange = (value) => {
+    setSearchInput(value);
+    fetchData(value);
+  };
   return (
     <div className="">
-      <SearchBar setResults={setResults} />
-      <SearchResultsList results={results} />
+      <div className="searchbar-wrap">
+        <span className="searchspan">
+          <input
+            type="text"
+            id="searchbar"
+            value={searchInput}
+            onChange={(e) => handleChange(e.target.value)}
+            placeholder="Search Student's Records"
+          />
+          <FontAwesomeIcon icon={faSearch} id="search-icon" />
+          <button
+            onClick={() => onSearch(searchInput)}
+            value="Search"
+            className="searchButton"
+          />
+        </span>
+      </div>
+      {results.map((results, id) => {
+        return (
+          <div
+            className="search-result"
+            onClick={() => setSearchInput(results.sname)}
+          >
+            {results.sname} <br /> {results.birthday}{" "}
+          </div>
+        );
+      })}
     </div>
   );
 }
