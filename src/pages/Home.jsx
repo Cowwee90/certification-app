6c3fbe62f06e491fa8a4805b01c48f9bcafd4ca8
Algebras <65031038+Algebras@users.noreply.github.com>
2023-07-26 22:41:17 -0600

    Reorganising files and move student button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
-
-import { StudentTable } from "../students/student-table.jsx";
+import { TestResultForm } from "../student-search-bar/test-result-form.jsx";
 import { EventTable } from "../events/event-table.jsx";
 import { TestResultTable } from "../test-results/test-result-table.jsx";
-import { AddStudentButton } from "../show-form.jsx";
-import { TestResultForm } from "../student-search-bar/test-result-form.jsx";
+
 
 const Home = () => {
   return (
     <div>
-      <AddStudentButton />
       <TestResultForm />
       <EventTable />
       <TestResultTable />
--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,14 +1,11 @@
 import { Link } from "react-router-dom";
-
 import { StudentTable } from "../students/student-table.jsx";
-import { EventTable } from "../events/event-table.jsx";
-import { TestResultTable } from "../test-results/test-result-table.jsx";
-import { AddStudentButton } from "../show-form.jsx";
-import { TestResultForm } from "../student-search-bar/test-result-form.jsx";
+import { AddStudentButton } from "../students/show-form.jsx";
 
 const Students = () => {
   return (
     <div>
+    <AddStudentButton />
       <StudentTable />
     </div>
   );
