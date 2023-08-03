eee81465443e7642a77c3148fa13ef7610cdb0d8
Algebras <65031038+Algebras@users.noreply.github.com>
2023-08-02 21:37:07 -0600

    more button css

--- src/admin-login/login.jsx
+++ src/admin-login/login.jsx
@@ -1,5 +1,6 @@
 import CryptoJS from "crypto-js";
 import { useState } from "react";
+import { Redirect } from 'react-router-dom';
 
 export default function LoginPage() {
   const [text, setText] = useState("");
@@ -38,6 +39,58 @@ export default function LoginPage() {
     if (screen === "encrypt") encryptData();
     else decryptData();
   };
+
+  const LoggedInPage = ({ setLoggedIn }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState('');
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        // send a request to the server to validate the credentials
+        const response = await fetch('/api/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+        if (response.ok) {
+          // set the user as logged in and store the token in local storage
+          const { token } = await response.json();
+          localStorage.setItem('token', token);
+          setLoggedIn(true);
+        } else {
+          setLoginError('Invalid username or password');
+        }
+      } catch (error) {
+        console.error(error);
+        setLoginError('An error occurred. Please try again later');
+      }
+    }
+
+    return (
+  <div>
+    <h1>Login</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+      {loginError && <div>{loginError}</div>}
+    </form>
+  </div>
+);
+};
+
 
   return (
     <div className="login"   style={{
